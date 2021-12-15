4d6aed19efd7a029d663cb79e3e71c4dc5dec06b
Andreas Lyngholm Poulsen <Lyngholmdk@gmail.com>
2021-12-15 10:32:50 +0100

    Clean

--- java_code/src/nuxt/runtime/App.java
+++ java_code/src/nuxt/runtime/App.java
@@ -2,11 +2,6 @@ package nuxt.runtime;
 
 public class App
 {
-	//public static StringBuilder stringBuilder = new StringBuilder();
-	//public static Map<String, String> data = new HashMap<String, String>();
-	//public static ArrayList<Map<String, String>> data = new ArrayList<Map<String, String>>();
-	//public static List<HashMap<String,String>> data = new ArrayList<HashMap<String,String>>();
-	
 //    public static void main( String[] args )
 //    {	
 //    	Compiler.compile();
